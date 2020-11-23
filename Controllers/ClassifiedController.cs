36eac4a09d6d6ef9b8bad9361b28f13fbe4213f6
RuanBezS <54983785+RuanBezS@users.noreply.github.com>
2020-11-23 22:21:18 +0200

    Add files via upload

---
+++ Controllers/ClassifiedController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppServices.Data;
+using WebAppServices.Models;
+
+namespace WebAppServices.Controllers
+{
+    public class ClassifiedController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ClassifiedController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            IEnumerable<Classified> objList = _db.Classified;
+            return View(objList);
+        }
+
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Classified obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Classified.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Classified.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Classified obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Classified.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Classified.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Classified.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Classified.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+
+    }
+}
+
