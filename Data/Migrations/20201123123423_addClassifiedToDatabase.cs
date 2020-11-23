58ced259ba5e9cb7a4cabaf567269fc9292134aa
RuanBezS <28551176@student.g.nwu.ac.za>
2020-11-23 20:06:10 +0200

    Add project files.

---
+++ Data/Migrations/20201123123423_addClassifiedToDatabase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppServices.Data.Migrations
+{
+    public partial class addClassifiedToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Classified",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classified", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Classified");
+        }
+    }
+}
---
+++ Models/Classified.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppServices.Models
+{
+    public class Classified
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+       // [DisplayName("Display Order")]
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Display Order for category must be greater than 0")]
+        public int Age { get; set; }
+        [Required, EmailAddress,StringLength(50)]
+        public string Email { get; set; }
+        [Required]
+        public string Gender { get; set; }
+
+    }
+}
